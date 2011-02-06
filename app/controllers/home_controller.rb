42993b851c7c27570125da530849f51e30b16d30
Nick Floyd <nicholas.floyd@gmail.com>
2011-02-06 10:25:51 -0600

    quick iter on adding gowalla call

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,12 @@
+require 'gowalla'
+require 'yaml'
+
 class HomeController < ApplicationController
   def index
+    gowalla = Gowalla::Client.new(:username => USER_NAME, :password => PASSWORD, :api_key => GOWALLA_KEY)
+    @user = gowalla.user('bryanmikaelian')
+    
+    
   end
 
 end
