821811e7118eede32472ea701b568324608f0892
Nick Floyd <nicholas.floyd@gmail.com>
2011-02-06 10:44:55 -0600

    added history

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,9 +4,9 @@ require 'yaml'
 class HomeController < ApplicationController
   def index
     gowalla = Gowalla::Client.new(:username => USER_NAME, :password => PASSWORD, :api_key => GOWALLA_KEY)
+  
     @user = gowalla.user('bryanmikaelian')
-    
-    
+    @checkins = gowalla.connection.get("/users/bryanmikaelian/checkins").body.events
   end
 
 end
